7436940803425b78a35d7a4b9fb985109342b423
Justin <jxm262@gmail.com>
2015-06-17 21:57:28 -0400

    adding test

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -2,4 +2,10 @@ package game;
 
 public class GameManager {
 
+	public void createHands(String[] black, String[] white) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- test/game/GameManagerTest.java
+++ test/game/GameManagerTest.java
@@ -7,9 +7,21 @@ import org.junit.Test;
 
 public class GameManagerTest {
 
+	GameManager gameManager = new GameManager();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createHands_obj_from_black_white_hand_inputs() {
+		String[] black = {"2H", "3D", "5S", "9C", "KD"};
+		String[] white = {"2C", "3H", "4S", "8C", "AH"};
+		
+		gameManager.createHands(black, white);
+		Hands expectedBlack;
+		Hands expectedWhite;
+		Hands actualBlack;
+		Hands actualWhite;
+		
+		assertSame("failure expected hands are not built correctly", expectedBlack, actualBlack);
+		assertSame("failure expected hands are not built correctly", expectedWhite, actualWhite);
 	}
 
 }
