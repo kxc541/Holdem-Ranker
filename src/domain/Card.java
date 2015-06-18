a41600d7855d837b28c2ed98f502274b1599ef23
kxc541 <kxc541@Kenny>
2015-06-17 21:14:16 -0400

    Committing Card object.

---
+++ src/domain/Card.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Card {
+	
+	String number;
+    String suit;
+    
+    public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+   
+}
